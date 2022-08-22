f06fecd3c6033c1fc0097a57e9d136c547d86f5e
Baraa Basim <34444872+BaraaBasim@users.noreply.github.com>
2022-08-22 10:31:52 +0300

    mario.c

---
+++ mario.c
@@ -0,0 +1,30 @@
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int input;
+    do
+    {
+        input = get_int("Height: ");
+    } while (input <= 0 || input > 8);
+
+    for (int i = 1; i <= input; i++)
+    {
+        for (int j = input - i; j > 0; j--)
+        {
+            printf(" ");
+        }
+        for (int k = i; k < i * 2; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int k = i; k < i * 2; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
